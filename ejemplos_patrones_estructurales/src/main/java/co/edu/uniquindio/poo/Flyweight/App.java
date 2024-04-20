ddd2f2eb528f0b25b4a237a03893fd07686acd59
JCamiloLopezF <130943645+JCamiloLopezF@users.noreply.github.com>
2024-04-20 09:13:44 -0500

    Carpetas creadas con su respectiva clase App

---
+++ ejemplos_patrones_estructurales/src/main/java/co/edu/uniquindio/poo/Flyweight/App.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.poo.Flyweight;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
